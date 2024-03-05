b633aaf7a618d0983fce8b555f1fe55da245f5aa
vishwanethra12 <vishwanethra7@gmail.com>
2024-03-05 17:14:44 +0530

    firstdemo

--- second/src/Parent.js
+++ second/src/Parent.js
@@ -1,13 +1,6 @@
 
 
 function Parent(){
-    let name=["vishwa","Nethra","Hey"];
-    let age=["20,"30","35"];
-    return (
-        na.map((n,index)=>
-            <child (na={n},age=age[index]/>)
-        }
-        
     
 
 
